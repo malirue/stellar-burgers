b56b4f611cc0d03c621bfac673e4129527119842
Evelina Ayrapetova <evelina.airapetova97@yandex.ru>
2026-04-07 16:25:46 +0300

    исправила ошибку из за которой проект зпаускался м ошибкой; при добавлении ингредиента в конструктор теперь добавляется уникальный uuid; хуки useSelector и useDispatch теперь импортируются из стора

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -1,6 +1,6 @@
 import { FC, memo } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import { v4 as randomUUID } from 'uuid';
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
 import { useAppDispatch } from '../../services/store';
@@ -15,7 +15,7 @@ export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
     const location = useLocation();
     const dispatch = useAppDispatch();
 
-    const handleAdd = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const handleAdd = (e: React.SyntheticEvent<Element, Event>) => {
       e.stopPropagation();
       if (ingredient.type === 'bun') {
         const bun: TBun = {
@@ -26,7 +26,7 @@ export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
       } else {
         const ingredientWithId: TConstructorIngredient = {
           ...ingredient,
-          id: ingredient._id
+          id: randomUUID()
         };
         dispatch(addIngredient(ingredientWithId));
       }
